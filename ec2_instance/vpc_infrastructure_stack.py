b894a5273c64951140bdfeee24d7c61bb6fc88fc
Amrut Chougale <chougale.amrut@gmail.com>
2020-05-06 20:53:01 -0400

    VPC with customer route table for 3 subnets in each 3 AZs

--- ec2_instance/vpc_infrastructure_stack.py
+++ ec2_instance/vpc_infrastructure_stack.py
@@ -2,34 +2,79 @@ from aws_cdk import (
     aws_ec2 as _ec2,
     core,
 )
+import hashlib
 
 
 class VpcInfrastructureStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+        self._ip_range = 0
 
-        # Subnet configuration for public, private subnets
-        web_subnets = _ec2.SubnetConfiguration(
-            subnet_type=_ec2.SubnetType.PUBLIC, cidr_mask=20,
-            name="web-"
+        # Crete a custom VPC
+        id = hashlib.md5(f'VPC'.encode()).hexdigest()
+        vpc = _ec2.CfnVPC(
+            self,
+            f'vpc-{id}',
+            cidr_block='10.0.0.0/16',
+            enable_dns_hostnames=True,
+            enable_dns_support=True,
+            instance_tenancy=None,
+            tags=[{'key': 'Name', 'value': 'My Lab VPC'}]
         )
 
-        # If SubnetType specified as PRIVATE it will create NATGW in each AZ
-        reserved_subnets = _ec2.SubnetConfiguration(
-            subnet_type=_ec2.SubnetType.ISOLATED,
-            cidr_mask=20, name="reserved-"
-        )
+        # TODO IPv6WorkAround for subnet cidr allocation.
+        # https://github.com/aws/aws-cdk/issues/894#issuecomment-606140766
+        assign_ipv6 = _ec2.CfnVPCCidrBlock(self, "testabd", vpc_id=vpc.ref,
+                                           amazon_provided_ipv6_cidr_block=True)
 
-        db_subnets = _ec2.SubnetConfiguration(
-            subnet_type=_ec2.SubnetType.ISOLATED,
-            cidr_mask=20,
-            name="db-"
-        )
-        # FIXME How to create custom RouteTable
+        # Create an Internet Gateway and attach it to the VPC
+        id = hashlib.md5(f'IGWVPC'.encode()).hexdigest()
+        igw = _ec2.CfnInternetGateway(self,
+                                      id=f'igw-{id}',
+                                      tags=[
+                                          {'key': 'Name', 'value': 'My Lab IGW'}]
+                                      )
+        igw_vpc_attach = _ec2.CfnVPCGatewayAttachment(self, id=f'IGWVPCAttach-{id}',
+                                                      vpc_id=vpc.ref,
+                                                      internet_gateway_id=igw.ref)
 
-        # VPC creation
-        my_vpc = _ec2.Vpc(
-            self, "my_vpc", max_azs=3,
-            subnet_configuration=[web_subnets, reserved_subnets, db_subnets],
-            cidr="10.16.0.0/16")
+        # Need to create 3 subnets per tier and attach it to custom route table
+        for sub_type in ["web", "reserved", "db"]:
+            id = hashlib.md5(f'RouteTable-{sub_type}'.encode()).hexdigest()
+            route_table = _ec2.CfnRouteTable(self,
+                                             f'rtb-{id}',
+                                             vpc_id=vpc.ref,
+                                             tags=[
+                                                 {'key': 'Name', 'value': f'RouteTable-{sub_type}'}])
+
+            # Create Route to internet via IGW for Web Subnets
+            if sub_type == 'web':
+                route_to_internet = _ec2.CfnRoute(self, f'routetointernet-{id}',
+                                                  route_table_id=route_table.ref,
+                                                  destination_cidr_block='0.0.0.0/0',
+                                                  gateway_id=igw.ref)
+
+            for i in range(0, 3):
+                id = hashlib.md5(f'Subnet-{sub_type}{i}'.encode()).hexdigest()
+                subnet = _ec2.CfnSubnet(
+                    self,
+                    id=f'subnet-{id}',
+                    cidr_block=f'10.0.{self._ip_range}.0/20',
+                    vpc_id=vpc.ref,
+                    availability_zone=core.Fn.select(
+                        i, core.Fn.get_azs(core.Aws.REGION)),
+                    tags=[
+                        {'key': 'Name', 'value': f'Subnet-{sub_type}{i + 1}'}])
+
+                self._ip_range = self._ip_range + 16
+
+                # Associate subnets with RouteTable
+                id = hashlib.md5(
+                    f'RouteTableAssoc-{sub_type}{i}'.encode()).hexdigest()
+                route_table_association = _ec2.CfnSubnetRouteTableAssociation(
+                    self,
+                    f'rtbassoc-{id}',
+                    route_table_id=route_table.ref,
+                    subnet_id=subnet.ref,
+                )
